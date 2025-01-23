3e4e31c9c19a32283fa4d89675fb86c970fe118a
Spacestarz <samromell96@gmail.com>
2025-01-23 15:35:39 +0100

    added touch input and mouse input if you click on card scale goes up WIP!

--- MobileGame/Assets/Scripts/TouchandMouseInputs.cs
+++ MobileGame/Assets/Scripts/TouchandMouseInputs.cs
@@ -1,16 +1,67 @@
 using UnityEngine;
+using DG.Tweening;
 
 public class TouchandMouseInputs : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //will test mouseinput and touch here
+    // watched a yt thing https://www.youtube.com/watch?v=aTtheFyh7Ac
+
+    private Vector2 _orgScaleCard;
+    private Vector2 _zoomScaleCard;
+
+    [SerializeField] private float _zoomScale = 0.5f;
+    [SerializeField] private float _zoomDuration = 2f;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
+
+        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            
+            Vector2 touchPosition = Camera.main.ScreenToWorldPoint(Input.touches[0].position);
+
+            RaycastHit2D hit = Physics2D.Raycast(touchPosition, Vector2.zero);
+
+            if (hit.collider != null)
+            {
+                Debug.Log($"touched object {hit.collider.name}");
+                
+            }
+        }
+        
+#if UNITY_EDITOR //if you are in the unity editor. Cool thing i am learning yay
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
+
+            if (hit.collider != null)
+            {
+                Debug.Log($"you touch/click {hit.collider.name}");
+
+                Transform cardtransform = hit.collider.transform;
+                _orgScaleCard = cardtransform.localScale;
+
+                //HAVE TO MAKE A LIMIt also
+                //make so the card dont go out of bound of the screen so it adapts to screen!
+
+                //also make so that card that zooms gets put above the other cards
+                cardtransform.DOScale(_orgScaleCard * _zoomScale, _zoomDuration);
+                
+            }
+            else
+            {
+                Debug.Log("you diden hit anything");
+            }
+        }
+#endif
         
     }
 }

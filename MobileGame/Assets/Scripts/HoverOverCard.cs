7ce66ae4686b3097d4f2dbadd3731cbe4258289e
Spacestarz <samromell96@gmail.com>
2025-01-28 11:44:20 +0100

    added comments from teacher more how to make card follow mouse etc

--- MobileGame/Assets/Scripts/HoverOverCard.cs
+++ MobileGame/Assets/Scripts/HoverOverCard.cs
@@ -11,8 +11,22 @@ public class HoverOverCard : MonoBehaviour
     private bool _CardhasbeenMoved = false;
     private GameObject CardClicked;
     private GameObject _lasthovocard;
+    
+    //on mouse press, if cardFound, heldCardRef = cardFound
+    // update, cardFound . position = mousePointer
+    // on mouse press, if heldCardRef != null, drop card (or return card to somewhere)
+    
+    // on mouse pressed, cardPickedUp = true
+    // if cPU == true, position = mousePointer //cPU = card pick up
+    // if pressed while CPU true, cpu false
+    // ScreenToWorldPosition
+    
     private void OnMouseOver() //have this script on cards and this event will trigger when hover over with mouse/held down touch
     {
+        
+        //todo KILL THE TWEEN
+        
+        //
         Debug.Log(gameObject.name);
         if (_CardhasbeenMoved && _lasthovocard == gameObject)
         {
@@ -26,6 +40,7 @@ public class HoverOverCard : MonoBehaviour
         newpos = _orgposCard.y + MovehowMuch;
     
         help = new Vector3(_orgposCard.x,newpos,_orgposCard.z);
+        
          //todo make card go up a bit
         gameObject.transform.DOMove(help, 0.1f);
         _CardhasbeenMoved = true;
@@ -36,6 +51,7 @@ public class HoverOverCard : MonoBehaviour
     {
         if (_CardhasbeenMoved && _lasthovocard == gameObject)
         {
+            //make a tween here too
             _lasthovocard.transform.localPosition = _orgposCard;
             _CardhasbeenMoved = false;
         }
